045c001ea622c116670e9b2753f7f8f3bccaa439
tripleg <tripleg.spa@gmail.com>
2024-12-15 20:22:51 -0300

    Implemente bin/render-build y render.yaml, previo a subir a render

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "welcome #index"
+  root "welcome#index"
 end
